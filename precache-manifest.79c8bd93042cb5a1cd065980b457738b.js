f129055e41bb91b45d7aed8ee683848238253d5d
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-02-11 10:26:33 -0500

    Updates

---
+++ precache-manifest.79c8bd93042cb5a1cd065980b457738b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4795c73f9746045815",
+    "url": "/board/static/css/main.57d8274b.chunk.css"
+  },
+  {
+    "revision": "be4795c73f9746045815",
+    "url": "/board/static/js/main.be4795c7.chunk.js"
+  },
+  {
+    "revision": "0a0f0b50ee2ab0a65648",
+    "url": "/board/static/js/runtime~main.0a0f0b50.js"
+  },
+  {
+    "revision": "aaa5e7ac662b5ac18b38",
+    "url": "/board/static/js/2.aaa5e7ac.chunk.js"
+  },
+  {
+    "revision": "864d69c951a689133dc708a6a1f6f730",
+    "url": "/board/index.html"
+  }
+];
