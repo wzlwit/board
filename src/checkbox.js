fb2562afd3a166d68b7389936ae2f4ad7730c612
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-02-14 13:07:37 -0500

    refactor

--- src/checkbox.js
+++ src/checkbox.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
 export class Checkbox extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { checked: false };
-        this.handleChecked = this.handleChecked.bind(this);
-    }
-    handleChecked() {
+    state = { checked: false }
+
+    handleChecked = () => {
         this.setState({ checked: !this.state.checked });
     }
     render() {
@@ -17,7 +14,7 @@ export class Checkbox extends Component {
                 <h2>Play with a checkbox</h2>
                 <input type='checkbox' onChange={this.handleChecked} defaultChecked={this.state.checked} />
                 <h3>Checkbox is "{msg}"</h3>
-                <br/>
+                <br />
             </div>
         );
     }
