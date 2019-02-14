5c2013911b1df5b7c59c493b2dc43c59087e782a
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-02-14 12:25:31 -0500

    Updates

---
+++ precache-manifest.23bb98f1f3c286680f143b57d36ddbf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "252e65d89476c2aea011",
+    "url": "/board/static/css/main.4858b61f.chunk.css"
+  },
+  {
+    "revision": "252e65d89476c2aea011",
+    "url": "/board/static/js/main.252e65d8.chunk.js"
+  },
+  {
+    "revision": "0a0f0b50ee2ab0a65648",
+    "url": "/board/static/js/runtime~main.0a0f0b50.js"
+  },
+  {
+    "revision": "6e96b77b8012de2696a9",
+    "url": "/board/static/js/2.6e96b77b.chunk.js"
+  },
+  {
+    "revision": "5e0f0e2cd1f22c9a0e2dbec00b606660",
+    "url": "/board/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/board/precache-manifest.9e388634be927bdfe8ef23afd66296bd.js"
+  "/board/precache-manifest.23bb98f1f3c286680f143b57d36ddbf4.js"
 );
 
 workbox.clientsClaim();
